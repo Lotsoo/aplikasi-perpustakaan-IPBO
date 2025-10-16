1269bfd71797f5f79a40785ff792d83f4cd39a2b
lotsoo <braindeathrama@gmail.com>
2025-10-16 21:00:21 +0700

    feat(dao): create DAO_Interface

---
+++ src/dao/DAO_Interface.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+import java.util.List;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public interface DAO_Interface<A> {
+   public void insert(A data);
+
+    public void update(A data);
+
+    public void delete(int key);
+
+    public List<A> getAll();
+
+    public List<A> getCari(String keyword);
+}
